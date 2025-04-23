fd2b1836bdbc409c1916e804f6cbe88e32251d7e
georgi-marchev <george_85_bg@yahoo.com>
2025-04-23 19:18:42 +0300

    Init project repo and server spring boot application

---
+++ chatroom-server/src/main/java/com/gmarchev/chatroom/server/ChatroomServerApplication.java
@@ -0,0 +1,13 @@
+package com.gmarchev.chatroom.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomServerApplication.class, args);
+	}
+
+}
